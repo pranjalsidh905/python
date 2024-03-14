ade62c1ffc43b7c49e6f7e3b62773e4552f06021
pranjalsidh905 <sidhpranjal905@gmail.com>
2024-03-14 16:45:40 +0530

    update

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8o353)z(_*^04+jhey!m*q3_no@vhn*7v8(6xz4rl6=kunu=it'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app',"now.sh"]
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
+STATICFITLES_DTRS = os.path.join(BASE_DIR,"static"),
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
